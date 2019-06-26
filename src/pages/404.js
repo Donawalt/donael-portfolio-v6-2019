767e814310470277d0acf6fba50465012e4665da
Donaël Walt <w.dodo.a@hotmail.fr>
2019-06-26 23:07:57 +0200

    fix org

--- src/pages/404.js
+++ src/pages/404.js
@@ -1,7 +1,7 @@
 import React from 'react'
 import Link from 'gatsby-link'
 
-import Layout from '../components/Layout'
+import Layout from '../components/layout'
 import '../style/notFound.scss'
 
 class NotFound extends React.Component {
