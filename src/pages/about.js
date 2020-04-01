da210f7e34e9cfb833904dc69c9795c0c690f37f
Donawalt <donael.walter@hotmail.com>
2020-04-01 15:50:09 +0200

    update helmet on page about

--- src/pages/about.js
+++ src/pages/about.js
@@ -2,7 +2,6 @@ import React from 'react'
 import { withPrefix } from 'gatsby'
 import {TweenLite, TimelineLite} from 'gsap'
 import Layout from '../components/layout'
-import Helmet from 'react-helmet';
 import SEO from '../components/seo'
 
 
